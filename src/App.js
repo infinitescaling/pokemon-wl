0936876231d6cf7d8b7b47fdca2ef2090695b05f
SPECTRiLL <spectrill@SPECTRiLLs-MacBook-Pro.local>
2023-09-24 19:01:01 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+// import logo from './logo.svg';
 import './App.css';
+import pokemonJSON from "./static/released_pokemon.json";
 
 function App() {
+  let availablePokemon = [];
+  const [pokemonPicker, setPokemonPicker] = useState("Select a Pokemon");
+  let handlePokemonchange = (e) => {
+    setPokemonPicker(e.target.value);
+    console.log(pokemonPicker);
+  }
+  const addToWishList = () => {
+    alert("Hello!");
+  };
+  // Push pokemon to Available Pokemon Array
+  Object.keys(pokemonJSON).forEach((key) =>
+  {
+    availablePokemon.push(pokemonJSON[key]);
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {pokemonPicker}
+        <select onChange={handlePokemonchange}> 
+          <option value="Select a Pokemon"> -- Select a Pokemon -- </option>
+          {
+            availablePokemon.map(
+              (pokemonPicker) => 
+                <option value={pokemonPicker.name}>{pokemonPicker.name}</option>
+              )
+          }
+        </select>
+        <Button variant="outline-primary" onClick={addToWishList}>Add</Button>
       </header>
+
     </div>
   );
 }
